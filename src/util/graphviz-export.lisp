9b6fbd7bc40fe4b13854a51eac52b0bd744ac2f0
John Kirollos <johnkirollos@gmail.com>
2024-11-10 03:41:57 +0200

    Graphviz export: added features (quick and dirty).
    Added features to Graphviz export (check documentation strings):
    
    - optional dumping DOT output to file and/or stream
    - optional description added to diagrams (e.g. regex itself)
    - style acceptance states differently (hardcoded green background)
    - new function that calls the Graphviz Dot program (all in one step)
    
    TODO: refactoring!

--- src/util/graphviz-export.lisp
+++ src/util/graphviz-export.lisp
@@ -73,9 +73,14 @@
 (defmethod element-to-edge ((element (eql :any-other-char)))
   "ANY-OTHER")
 
-(defun fsm-to-graphvizdot (root-fsm-state)
-  "Generate a Graphviz DOT string for an NFA or DFA state machine, starting at
-ROOT-FSM-STATE."
+(defun fsm-to-graphvizdot (root-fsm-state &key (output-file nil) (output-stream nil) (regex nil))
+  "Generate a Graphviz DOT string for an NFA or DFA state machine, starting at ROOT-FSM-STATE. The
+generated string is returned, and is also optionally dumped to specified output file and/or output
+stream. The optional REGEX parameter is also included in the diagram as a description for the regex.
+Two keyword options also control the dumping:
+OUTPUT-FILE specifies the output file path, and is NIL by default (no dumping to output file).
+OUTPUT-STREAM specifies a stream to dump to (besides the output file). If T is passed, standard
+output is used. NIL (default) disables the dumping."
   (labels ((fsm-transitions-to-dot (output-stream)
              (let ((state-index -1)
                    (state-lookup (make-hash-table)))
@@ -83,13 +88,41 @@ ROOT-FSM-STATE."
                           (or (gethash state state-lookup)
                               (setf (gethash state state-lookup) (incf state-index))))
                         (transition-to-dot (src elem dst)
+                          ;;TODO: REFACTOR!!
+                          (when (fsm:fsm-acceptance-state-p src)
+                            (format output-stream "~%   ~a [style=filled, fillcolor=~a];"
+                                    (get-state-index src)
+                                    "lightgreen"))
+                          (when (fsm:fsm-acceptance-state-p dst)
+                            (format output-stream "~%   ~a [style=filled, fillcolor=~a];"
+                                    (get-state-index dst)
+                                    "lightgreen"))
                           (format output-stream "~%    ~a -> ~a [label=\"~a\"];"
                                   (get-state-index src)
                                   (get-state-index dst)
                                   (element-to-edge elem))))
                  (fsm:traverse-fsm-transitions root-fsm-state #'transition-to-dot)))))
-    (with-output-to-string (output-stream)
-      (format output-stream "digraph \{~%")
-      (format output-stream "rankdir = LR;~%")
-      (fsm-transitions-to-dot output-stream)
-      (format output-stream "~%\}~%"))))
+    (let ((result (with-output-to-string (output-stream)
+                    (format output-stream "digraph \{~%")
+                    (format output-stream "rankdir = LR;~%")
+                    (when regex
+                      (format output-stream "~%description [label=\"~a\", shape=box, style=filled,"
+                              regex)
+                      (princ  "color=lightyellow];" output-stream))
+                    (fsm-transitions-to-dot output-stream)
+                    (format output-stream "~%\}~%"))))
+      (when output-file
+        (with-open-file (file-stream output-file :direction :output :if-exists :supersede)
+          (princ result file-stream)))
+      (when output-stream
+        (format output-stream "Generated DOT output:~%~a" result))
+      result)))
+
+(defun generate-graphviz-dot-diagram (root-fsm-state output-file &optional regex)
+  "Generate diagram for an FSM (whether NFA or DFA), given its root state (ROOT-FSM-STATE). The
+output is written to a file whose path is specified by the OUTPUT-FILE. An optional argument REGEX
+may be used to describe the regex, to be displayed in the diagram."
+  (let* ((dot (fsm-to-graphvizdot root-fsm-state :regex regex))
+         (command (format nil "echo ~s | dot  -Tsvg -Nfontcolor=red -Nshape=circle -o ~a"
+                          dot output-file)))
+    (uiop:run-program command :output *standard-output*)))
--- src/util/packages.lisp
+++ src/util/packages.lisp
@@ -1,6 +1,7 @@
 (defpackage :parsex-cl.graphviz-util
   (:use #:cl)
   (:local-nicknames (:chars #:parsex-cl.chars)
-  (:export #:fsm-to-graphvizdot))
                     (:elm #:parsex-cl.regex-element)
                     (:fsm #:parsex-cl.regex-fsm))
+  (:export #:fsm-to-graphvizdot
+           #:generate-graphviz-dot-diagram))
