a44a191addb8b4569e8923dee69025217fa3874e
John Kirollos <johnkirollos@gmail.com>
2024-12-22 22:35:55 +0200

    Update regex sexp parsing to allow reading element tags in any package.
    With this change, regex tags (SEQ, CHAR-RANGE, OR etc.) do not have to be
    keywords any more, so a regex could be defined like this:
    
    (seq #\a (char-range #\x #\z))
    
    instead of this:
    
    (:seq #\a (:char-range #\x #\z))
    
    The reason is to make typing regex a little bit easier. Note however that
    using keywords is still also allowed (since I'm now allowing to read those
    symbols in any package). That's why I don't have to update the unit tests
    in this revision.
    
    I also defined a macro, to avoid repeating the prefix of the package name
    in each ECASE line.
    
    I may review/simplify in later revisions.

--- src/regex-sexp.lisp
+++ src/regex-sexp.lisp
@@ -2,6 +2,27 @@
 
 ;;; Handling of regex in the form of sexp (sexp --> regex object tree).
 
+(defconstant +regex-element-tags-package+ (let ((pkg-name :regex.element.tags))
+                                            (or (find-package pkg-name)
+                                                (make-package pkg-name)))
+  "Package dedicated for regex tag names (SEQ, OR, +, etc.).")
+
+;; TODO: reusable utility, or use Alexandria's ENSURE-SYMBOL
+(defun reintern (symbol package)
+  "Intern a symbol SYMBOL into a package PACKAGE, and return the newly interned symbol.
+The difference from the standard INTERN function is that it receives a symbol, not a symbol name. So
+practically it copies a symbol from a package into another one."
+  (intern (symbol-name symbol) (find-package package)))
+
+(defmacro case-list-regex (element-tag &body body)
+  (labels ((prepare-body (body)
+             (loop for (each-tag . each-clause) in body
+                   collect (cons (reintern each-tag +regex-element-tags-package+) each-clause))))
+    (let ((reinterned-element-tag (gensym)))
+      `(let ((,reinterned-element-tag (reintern ,element-tag +regex-element-tags-package+)))
+         (ecase ,reinterned-element-tag
+           ,@(prepare-body body))))))
+
 ;;;; NOTE: since the reader will accept only a valid sexp, the "no more input" case is not possible.
 ;;;; TODO: ensure correct number of elements for each type (e.g. * accepts 1 & only 1 element)
 ;;;; TODO: may split function using other helper functions
@@ -12,7 +33,9 @@
   (etypecase regex
     (character (make-instance 'elm:single-char-element :single-char regex))
     (keyword regex) ;"any char" (TODO: pass unchanged, or generate any-char-element?)
-    (list (ecase (car regex)
+    (list (case-list-regex (car regex)
+            ;; note that I could use any symbols (no need for keywords), but keywords are better to
+            ;; avoid confusing the syntax coloring
             (:char-range (make-instance 'elm:char-range-element
                                         :char-start (second regex)
                                         :char-end (third regex)))
