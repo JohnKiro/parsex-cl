0760bda1391afab2f33998cfcc348569b192983e
John Kirollos <johnkirollos@gmail.com>
2025-08-29 04:38:03 +0300

    Remove unnecessary base class, apply new slot naming convention, reindentation.
    Remove the unnecessary `element` base class, since there is no logic generic
    enough to be implemented at that level.
    
    Update the class slot naming convention started to follow recently
    (% prefix).
    
    Reindent the file.

--- src/regex/regex-element.lisp
+++ src/regex/regex-element.lisp
@@ -6,7 +6,7 @@
 Other element types not needing special class:
 - symbols (currently only :any-char (corresponds to . in regex).
 - single character (for now, I'm encapsulating it in single-char-element, mainly to group
-  it together with char-range-element as simple-element).
+it together with char-range-element as simple-element).
 - string (string of character corresponds to a sequence-element where all elements are characters).
 ||#
 
@@ -21,27 +21,22 @@ negation and inversion, and for now, I'm not allowing it for the user. Later, I 
 that user could define a regex like (or #\a (seq any-other-char #\x)). In such case, 'bx' would
 match, but not 'ax'.")
 
-;; TODO: remove (no need for base abstract)
-(defclass element () ()
-  (:documentation "Base class for all regex element types."))
-
-;;; Single character elements (range, any-char, specific char)
-(defclass simple-element (element) ()
+(defclass simple-element () ()
   (:documentation "Base class for single char regex elements (single char and char range)."))
 
 ;;; TODO: probably remove (use character directly). The problem is that I would have two
 ;;; identical produce-nfa methods :(
 ;;; Also may need an object, to include some properties, such as "accepted token" (not sure).
 (defclass single-char-element (simple-element)
-  ((single-char :initarg :single-char :initform (error "Mandatory")
-                :reader single-char :type character))
+  ((%single-char :initarg :single-char :initform (error "Mandatory")
+                 :reader single-char :type character))
   (:documentation "Wrapper object for single-char elements."))
 
 (defclass char-range-element (simple-element)
-  ((char-start :initarg :char-start :initform (error "Mandatory")
-               :reader char-start :type character)
-   (char-end :initarg :char-end :initform (error "Mandatory")
-             :reader char-end :type character))
+  ((%char-start :initarg :char-start :initform (error "Mandatory")
+                :reader char-start :type character)
+   (%char-end :initarg :char-end :initform (error "Mandatory")
+              :reader char-end :type character))
   (:documentation "Char range regex element."))
 
 (defmethod print-object ((object single-char-element) stream)
@@ -50,11 +45,11 @@ match, but not 'ax'.")
 
 (defmethod print-object ((object char-range-element) stream)
   (print-unreadable-object (object stream :type t :identity nil)
-    (with-slots ((s char-start) (e char-end)) object
+    (with-slots ((s %char-start) (e %char-end)) object
       (format stream "(~a - ~a)" s e ))))
 
 (defmethod initialize-instance :after ((element char-range-element) &key)
-  (with-slots (char-start char-end) element
+  (with-slots ((char-start %char-start) (char-end %char-end)) element
     (when (char> char-start char-end)
       (let ((tmp char-start))
         (setf char-start char-end)
@@ -97,10 +92,10 @@ Preconditions: end >= start, splitting points must be a sorted vector."
         (end (char-end char-range-element))
         (acc nil))
     (loop for p across splitting-points
-      ;; skip splitting points < slider (effective at start, till splitting points start to
-      ;; overlap with the range at hand, noting that splitting points are assumed sorted).
-      ;; However, since we're doing this check for all splitting points, those that are not in
-      ;; order will be skipped. Alternatively, I may have separate loop to skip points < start.
+          ;; skip splitting points < slider (effective at start, till splitting points start to
+          ;; overlap with the range at hand, noting that splitting points are assumed sorted).
+          ;; However, since we're doing this check for all splitting points, those that are not in
+          ;; order will be skipped. Alternatively, I may have separate loop to skip points < start.
           do (when (char>= p slider)
                (if (char< p end)
                    (progn
@@ -115,13 +110,13 @@ Preconditions: end >= start, splitting points must be a sorted vector."
     acc))
 
 (defclass single-element-wrapper-element ()
-  ((element :initarg :element :initform (error "Mandatory") :reader inner-element)))
+  ((%element :initarg :element :initform (error "Mandatory") :reader inner-element)))
 
 (defclass negated-element (single-element-wrapper-element)
-  ((greedy :initarg :greedy
-           :initform nil
-           :reader greedy-p
-           :type boolean)))
+  ((%greedy :initarg :greedy
+            :initform nil
+            :reader greedy-p
+            :type boolean)))
 
 (defclass zero-or-more-element (single-element-wrapper-element) ())
 
@@ -138,8 +133,8 @@ Preconditions: end >= start, splitting points must be a sorted vector."
 ;;; TODO: not currently used (actually instead, it's a parameter in DFA state)
 (defclass any-other-char-element (simple-element) ())
 
-(defclass multiple-elements-wrapper-element (element)
-  ((elements :initarg :elements :initform nil :reader inner-elements :type sequence)))
+(defclass multiple-elements-wrapper-element ()
+  ((%elements :initarg :elements :initform nil :reader inner-elements :type sequence)))
 
 (defclass sequence-element (multiple-elements-wrapper-element) ())
 
