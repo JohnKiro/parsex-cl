b2898ab2233b8189f3a23b8c86b2bc815f0e2e80
John Kirollos <johnkirollos@gmail.com>
2025-10-18 02:07:57 +0300

    Refactoring: remove unnecessary local function since it only calls another function.
    Simplify `regex-to-nfa` for negation by removing a local function of little use, since it
    only calls another function. This removes one level of nesting and enhances readability.

--- src/regex/regex-nfa.lisp
+++ src/regex/regex-nfa.lisp
@@ -77,6 +77,9 @@ and returns output state as continuation point."))
     output-state))
 
 (defmethod regex-to-nfa ((regex elm:negated-element) input-nfa-state)
+  "Testing new implementation of ANALYZE-NFA-STATE-REACHABILITY -- VERY PROMISING. I will build on
+this one from now on."
+  (format t "DEBUG: Regex to NFA for a negation, input state: ~a~&" input-nfa-state)
   (let* ((inner-regex (elm:inner-element regex))
          (output-state-inner (regex-to-nfa inner-regex input-nfa-state))
          (glue-state (make-instance 'state:nfa-state))
@@ -87,74 +90,72 @@ and returns output state as continuation point."))
                            glue-state))
          (state-reachability (state:analyze-nfa-state-reachability input-nfa-state
                                                                    output-state-inner)))
-    (labels ((cleanup-dead-paths-on-auto (orig-state)
-               "Cleanup auto transitions, where destination is the output-state-inner."
-               (state:delete-auto-transition orig-state output-state-inner)))
-      ;; traverse NFA sub-tree, and states that have output-state-inner in their closures will
-      ;; be converted to dead-ends, and states that can reach output-state-inner via elements
-      ;; will get a transition on any-char to output-state (later: optionally via a
-      ;; (+ any-char))
-      (let ((inner-cont-pts-closures (state:prepare-nfa-state-closure-union
-                                      (loop for state-i being the hash-keys in state-reachability
-                                              using (hash-value s-status)
-                                            when (eq s-status :auto-connected) collect state-i))))
-        (loop for state-i being the hash-keys in state-reachability using (hash-value s-status)
-              do (ecase s-status
-                   (:auto-connected
-                    (state:set-dead-end state-i)
-                    ;; just simplification, not needed (cleans up generated NFA, but DFA not
-                    ;; affected, test cases pass)
-                    (cleanup-dead-paths-on-auto state-i))
-                   (:element-connected
-                    (state:set-nfa-transition-on-any-other state-i glue-state)
-                    (unless (member state-i inner-cont-pts-closures :test #'eql)
-                      ;; TODO: give user the choice (greedy/non-greedy)
-                      (state:add-nfa-auto-transition state-i output-state)
-                      (state:unset-dead-end state-i)))
-                   (:not-connected
+    ;; traverse NFA sub-tree, and states that have output-state-inner in their closures will
+    ;; be converted to dead-ends, and states that can reach output-state-inner via elements
+    ;; will get a transition on any-char to output-state (later: optionally via a
+    ;; (+ any-char))
+    (let ((inner-cont-pts-closures (state:prepare-nfa-state-closure-union
+                                    (loop for state-i being the hash-keys in state-reachability
+                                            using (hash-value s-status)
+                                          when (eq s-status :auto-connected) collect state-i))))
+      (loop for state-i being the hash-keys in state-reachability using (hash-value s-status)
+            do (ecase s-status
+                 (:auto-connected
+                  (state:set-dead-end state-i)
+                  ;; Cleanup auto transitions, where destination is the output-state-inner.
+                  ;; This is just simplification, not needed (cleans up generated NFA, but DFA not
+                  ;; affected, test cases pass)
+                  (state:delete-auto-transition state-i output-state-inner))
+                 (:element-connected
+                  (state:set-nfa-transition-on-any-other state-i glue-state)
+                  (unless (member state-i inner-cont-pts-closures :test #'eql)
+                    ;; TODO: give user the choice (greedy/non-greedy)
                     (state:add-nfa-auto-transition state-i output-state)
-                    (state:unset-dead-end state-i)))))
-      (fsm:with-unique-visit (state input-nfa-state add-inversion-transitions)
-        (let ((closure (state:prepare-nfa-state-closure-union (list state))))
-          (if (state::states-have-trans-on-any-other-p closure)
-              (progn
-                ;; clear "any-other" transitions from the whole closure, since
-                ;;we'll convert them to normal transitions
-                ;; TODO: try to avoid having to do this (merge with 1st pass?)
-                (dolist (s closure)
-                  (state:unset-nfa-transition-on-any-other s))
-                ;; convert "any-other" transitions into normal transitions (using
-                ;; inversion)
-                ;; note that we add the created transitions to the closure's
-                ;; initial point (actually it doesn't matter which state in the
-                ;; closure gets the transitions)
-                (let ((splitting-pts (state::collect-char-range-splitting-points closure))
-                      (split-elements nil))
-                  (format t "DEBUG: Splitting points: ~s.~&" splitting-pts)
-                  (state::do-normal-transitions (_ element next-state) closure
-                    ;; check to avoid inverting an inverted element
-                    (unless  (eq next-state glue-state)
-                      (etypecase element
-                        (elm:char-range-element
-                         (let ((split-ranges (elm:split-char-range element splitting-pts)))
-                           (dolist (r split-ranges)
-                             (push r split-elements))))
-                        (elm:single-char-element
-                         (push element split-elements)))))
-                  (loop for inv-elem in (elm::invert-elements
-                                         (elm::sort-simple-elements split-elements))
-                        do (state:add-nfa-normal-transition state inv-elem glue-state))))
-              (progn
-                ;; else: no any-other trans, => traverse closure
-                (dolist (s closure)
-                  (add-inversion-transitions s))))
-          ;; traverse normal transitions (TODO: and ANY-CHAR transitions as
-          ;; well?? Not making difference, since inverting any-char gives
-          ;; "no char")
-          (state::do-normal-transitions (trans elm next-state) closure
-            (add-inversion-transitions next-state))))
-      ;; connect the NOT element to the rest of the NFA
-      output-state)))
+                    (state:unset-dead-end state-i)))
+                 (:not-connected
+                  (state:add-nfa-auto-transition state-i output-state)
+                  (state:unset-dead-end state-i)))))
+    (fsm:with-unique-visit (state input-nfa-state add-inversion-transitions)
+      (let ((closure (state:prepare-nfa-state-closure-union (list state))))
+        (if (state::states-have-trans-on-any-other-p closure)
+            (progn
+              ;; clear "any-other" transitions from the whole closure, since
+              ;;we'll convert them to normal transitions
+              ;; TODO: try to avoid having to do this (merge with 1st pass?)
+              (dolist (s closure)
+                (state:unset-nfa-transition-on-any-other s))
+              ;; convert "any-other" transitions into normal transitions (using
+              ;; inversion)
+              ;; note that we add the created transitions to the closure's
+              ;; initial point (actually it doesn't matter which state in the
+              ;; closure gets the transitions)
+              (let ((splitting-pts (state::collect-char-range-splitting-points closure))
+                    (split-elements nil))
+                (format t "DEBUG: Splitting points: ~s.~&" splitting-pts)
+                (state::do-normal-transitions (_ element next-state) closure
+                  ;; check to avoid inverting an inverted element
+                  (unless  (eq next-state glue-state)
+                    (etypecase element
+                      (elm:char-range-element
+                       (let ((split-ranges (elm:split-char-range element splitting-pts)))
+                         (dolist (r split-ranges)
+                           (push r split-elements))))
+                      (elm:single-char-element
+                       (push element split-elements)))))
+                (loop for inv-elem in (elm::invert-elements
+                                       (elm::sort-simple-elements split-elements))
+                      do (state:add-nfa-normal-transition state inv-elem glue-state))))
+            (progn
+              ;; else: no any-other trans, => traverse closure
+              (dolist (s closure)
+                (add-inversion-transitions s))))
+        ;; traverse normal transitions (TODO: and ANY-CHAR transitions as
+        ;; well?? Not making difference, since inverting any-char gives
+        ;; "no char")
+        (state::do-normal-transitions (trans elm next-state) closure
+          (add-inversion-transitions next-state))))
+    ;; connect the NOT element to the rest of the NFA
+    output-state))
 
 
 (defmethod regex-to-nfa% ((regex elm:inv-element) input-nfa-state)
