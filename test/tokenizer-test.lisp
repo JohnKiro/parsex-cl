2ea4e2ea0e31b02672b58c27975a6a6228999156
John Kirollos <johnkirollos@gmail.com>
2026-03-31 02:56:57 +0200

    Tokenizer test: + suite name, + optional status and consumed text, + tests.
    - enhance test suite name, follwing latest convention (within this package
      rather than keyword package).
    - add optional test parameters to be validated if provided in expected data:
      status (still not provided in tokenization result - TBD), consumed text.
    - add test cases for tokenization errors (at start, in the middle), and a
      test case for backtracking (e.g. "=" VS "==").

--- test/tokenizer-test.lisp
+++ test/tokenizer-test.lisp
@@ -1,26 +1,34 @@
 (in-package :parsex-cl.test/tokenizer.test)
 
 ;; Test suite to contain all tokenizer tests
-(def-suite :parsex-cl/tokenizer.test-suite
+(def-suite tokenizer.test-suite
   :description "Tests both source-backed tokenizer and backtracking tokenizer."
   :in :parsex-cl.test-suite)
 
 ;;; All tests below to be part of tokenizer-tests
-(in-suite :parsex-cl/tokenizer.test-suite)
+(in-suite tokenizer.test-suite)
 
-(defun run-source-backed-tokenization-test-loop (tokenizer-core-dfa input-text expected-tokens)
+(defun run-source-backed-tokenization-test-loop (tokenizer-core-dfa input-text expected-results)
   "Runs tokenization loop on `input-text` against tokenizer core `tokenizer-core-dfa`, expecting a series
-of tokens described by `expected-tokens`, which is a list with each element in the form:
-(expected-token-id expected-token-text)."
+of tokens described by `expected-results`, which is a list with each element in the form:
+(expected-token-id expected-token-text expected-status expected-consumed-text), with the last two
+parameters optional."
   (let* ((input (input:create-basic-regex-input input-text))
          (tokenizer-fn (tokenizer:create-source-backed-tokenizer tokenizer-core-dfa input)))
     (loop for tokenizer-result = (funcall tokenizer-fn)
-          for (token-id . token-text) = (cons (and tokenizer-result (aref tokenizer-result 0))
-                                              (input:retrieve-last-accumulated-value input))
-          for (expected-token-id expected-token-text) in expected-tokens
-          do (fiveam:is (equal token-id expected-token-id))
-             (fiveam:is (equal token-text expected-token-text))
-          until (null token-id))))
+          for token-id = (and tokenizer-result (aref tokenizer-result 0))
+          for token-text = (input:retrieve-last-accumulated-value input)
+          for status = :tbd
+          for consumed-text = (input:retrieve-last-consumed-value input)
+          for expected-result in expected-results
+          for (exp-token-id exp-token-text exp-status exp-consumed-text) = expected-result
+          do (fiveam:is (equal token-id exp-token-id))
+             (fiveam:is (equal token-text exp-token-text))
+             ;; check only if specified in expected
+             (when (>= (length expected-result) 3)
+               (fiveam:is (equal status exp-status)))
+             (when (>= (length expected-result) 4)
+               (fiveam:is (equal consumed-text exp-consumed-text))))))
 
 (defmacro deftest (name &key desc tokenizer-core-dfa input-text expected-tokens)
   "Define a test case for tokenization loop on `input-text` against tokenizer `tokenizer-core-dfa`,
@@ -58,8 +66,8 @@ tokenizer core macro."
                     (nil nil)))
 
 (deftest source-backed-tokenizer-test-2
-  :desc "Basic source-backed tokenizer test for the happy scenario (no invalid input characters), using
-tokenizer sexp macro instead."
+  :desc "Basic source-backed tokenizer test for the happy scenario (no invalid input characters), this
+time using tokenizer sexp macro (preferred, since it hides regex processing, hence shorter syntax)."
   :tokenizer-core-dfa (token-core:tokenizer
                         (token:token assign-op "=")
                         (token:token mul-op "*")
@@ -81,5 +89,74 @@ tokenizer sexp macro instead."
                     (add-op "+")
                     (num "11")
                     (right-paren ")")
+                    (statement-term ";")
+                    (nil nil)))
+
+(deftest source-backed-tokenizer-test-3
+  :desc "Source-backed tokenizer test, with error encountered at start of tokenization."
+  :tokenizer-core-dfa (token-core:tokenizer
+                        (token:token assign-op "=")
+                        (token:token mul-op "*")
+                        (token:token add-op "+")
+                        (token:token left-paren #\()
+                        (token:token right-paren #\))
+                        (token:token statement-term ";")
+                        (token:token id (seq #1=(char-range #\A #\z)
+                                             (* (or #1#
+                                                    (char-range #\0 #\9)))))
+                        (token:token num (+ (char-range #\0 #\9))))
+  :input-text "id1=id2*&#$(id3+11);"
+  :expected-tokens ((id "id1")
+                    (assign-op "=")
+                    (id "id2")
+                    (mul-op "*")
+                    (nil nil :tbd "&")
+                    (nil nil :tbd "#")
+                    (nil nil :tbd "$")
+                    (left-paren "(")
+                    (id "id3")
+                    (add-op "+")
+                    (num "11")
+                    (right-paren ")")
+                    (statement-term ";")
+                    (nil nil)))
+
+(deftest source-backed-tokenizer-test-4
+  :desc "Source-backed tokenizer test, with error encountered in the middle of tokenization."
+  :tokenizer-core-dfa (token-core:tokenizer
+                        (token:token equality-op "==")
+                        (token:token statement-term ";")
+                        (token:token id (seq #1=(char-range #\A #\z)
+                                             (* (or #1#
+                                                    (char-range #\0 #\9))))))
+  :input-text "id1=#id2;id3==id4;"
+  :expected-tokens ((id "id1")
+                    (nil nil :tbd "=")
+                    (nil nil :tbd "#")
+                    (id "id2")
+                    (statement-term ";")
+                    (id "id3")
+                    (equality-op "==" :tbd "==")
+                    (id "id4")
+                    (statement-term ";")
+                    (nil nil)))
+
+(deftest source-backed-tokenizer-test-5
+  :desc "Source-backed tokenizer test, backtracking in the middle of tokenization."
+  :tokenizer-core-dfa (token-core:tokenizer
+                        (token:token assign-op "=")
+                        (token:token equality-op "==")
+                        (token:token statement-term ";")
+                        (token:token id (seq #1=(char-range #\A #\z)
+                                             (* (or #1#
+                                                    (char-range #\0 #\9))))))
+  :input-text "id1=id2;id3==id4;"
+  :expected-tokens ((id "id1")
+                    (assign-op "=" :tbd "=")
+                    (id "id2")
+                    (statement-term ";")
+                    (id "id3")
+                    (equality-op "==" :tbd "==")
+                    (id "id4")
                     (statement-term ";")
                     (nil nil)))
